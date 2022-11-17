48d490dd800a64f851701579e89b96b61f323b77
maxshend <shendrik.mv@gmail.com>
2022-11-18 01:26:05 +0300

    Add attachment variant for page cover.

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,7 +2,9 @@
 
 class Page < ApplicationRecord
   belongs_to :user
-  has_one_attached :cover
+  has_one_attached :cover do |attachable|
+    attachable.variant :thumb, resize_to_limit: [250, 250]
+  end
 
   validates :title, presence: true
 end
